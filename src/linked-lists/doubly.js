cb573e154f864a88966c630cd44f2d4534b30fc1
“malcolmmutare” <mutarem@gmail.com>
2025-02-25 10:20:37 +0200

    feat: added doubly.js

---
+++ src/linked-lists/doubly.js
@@ -0,0 +1,148 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+export class DoublyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    append(value) {
+        if (this.head === null) {
+            this.head = new Node(value)
+            this.tail = this.head
+            this.length++
+        }else {
+            const newNode = new Node(value)
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            newNode.next = null
+            this.tail = newNode
+            this.length++
+        }
+    }
+
+    prepend(value) {
+        if (this.head === null) {
+            this.head = new Node(value)
+            this.tail = this.head
+            this.length++
+        }else {
+            const newNode = new Node(value)
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+            this.length++
+        }
+    }
+
+    removeLast() {
+        if (this.head === null) {
+            return null
+        } else if (this.head.value === this.tail.value) {
+            const removed = this.tail
+            this.head = null
+            this.tail = null
+            this.length--
+            
+            
+            return removed
+        }
+    }
+
+    insert(index, value) {
+        if (this.head === null) {
+            return null
+        }
+
+        const isIndexOutOfBounds = index < 0 || index > this.length
+
+        if (isIndexOutOfBounds) {
+            return null
+        }
+
+        let nodeBeforeIndex = this.head
+        for (let i = 0; i < index - 1; i++) {
+            nodeBeforeIndex = nodeBeforeIndex.next
+        }
+
+        const newNode = new Node(value)
+
+        nodeBeforeIndex.next.prev = newNode
+        newNode.next = nodeBeforeIndex.next.prev
+
+        nodeBeforeIndex.next = newNode
+        newNode.prev = nodeBeforeIndex
+
+        this.length++
+    }
+
+    removeAt(index) {
+        if (this.head === null) {
+            return null
+        }
+
+        const isIndexOutOfBounds = index < 0 || index > this.length
+
+        if (isIndexOutOfBounds) {
+            return null
+        }
+
+        let nodeBeforeIndex = this.head
+        for (let i = 0; i < index -1; i++) {
+            nodeBeforeIndex = nodeBeforeIndex.isIndexOutOfBounds
+        }
+
+        const removed = nodeBeforeIndex.next
+
+        nodeBeforeIndex.next.next.prev = nodeBeforeIndex
+        nodeBeforeIndex.next = nodeBeforeIndex.next.next
+
+        this.length--
+        return removed
+    }
+
+    find(value) {
+        if (this.head === null) return null
+        
+        let currentNode = this.head
+
+        while (currentNode !== null) {
+            if (currentNode.value === value) {
+                return currentNode
+            }
+
+            currentNode = currentNode.next
+        }
+
+        return null
+    }
+
+    toArray() {
+        if (this.head === null) return []
+
+        let array = []
+
+        let currentNode = this.head
+
+        while (currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+
+        return array
+    }
+
+    clear() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+}
