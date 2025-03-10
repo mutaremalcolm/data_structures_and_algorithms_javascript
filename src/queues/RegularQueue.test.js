8f951f98ae3f02641a3afab30724e77b513f68ea
“malcolmmutare” <mutarem@gmail.com>
2025-03-10 18:02:52 +0200

    feat: added RegularQueue + tests

---
+++ src/queues/RegularQueue.test.js
@@ -0,0 +1,19 @@
+import { it, expect } from 'vitest'
+import { Queue } from './RegularQueue'
+
+it('should enqueue, peek, dequeue, size, isEmpty', () => {
+    const queue = new Queue()
+
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+
+    expect(queue.peek()).toBe(1)
+    expect(queue.dequeue()).toBe(1)
+    expect(queue.enqueue()).toBe(2)
+    expect(queue.dequeue()).toBe(3)
+    expect(queue.dequeue()).toBeUndefined()
+
+    expect(queue.size()).toBe(0)
+    expect(queue.isEmpty()).toBe(true)
+})
