51967a270a8a2ae40047b80502df443bba80e39c
“malcolmmutare” <mutarem@gmail.com>
2025-03-12 13:13:10 +0200

    feat: added binary-tree + tests

---
+++ src/trees/binary-tree.js
@@ -0,0 +1,116 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+export class BinaryTree {
+    constructor() {
+        this.root = null
+    }
+
+    isEmpty() {
+        return this.root === null
+    }
+
+    insert(value) { 
+        const newNode = new Node(value)
+
+        if (this.isEmpty()) {
+            this.root = newNode
+            return
+        }
+
+        const queue = [this.root]
+
+        while (queue.length) {
+            const currentNode = queue.pop()
+
+            if (currentNode.right && currentNode.left) {
+                queue.push(currentNode.right)
+                queue.push(currentNode.left)
+                continue
+            }
+
+            if (!currentNode.left) {
+                currentNode.left = newNode
+                break
+            }
+
+            if (!currentNode.right) {
+                currentNode.right = newNode
+                break
+            }
+        }
+    }
+
+    preorderTraversal(currentNode = this.root, result = []) {
+        if (!currentNode) return result
+
+        result.push(currentNode.value)
+        this.preorderTraversal(currentNode.left, result)
+        this.preorderTraversal(currentNode.right, result)
+
+        return result
+    }
+
+    inOrderTraversal(currentNode = this.root, result = []) {
+        if (!currentNode) return result
+
+        this.inOrderTraversal(currentNode.left, result)
+        result.push(currentNode.value)
+        this.inOrderTraversal(currentNode.right, result)
+
+        return result
+    }
+
+    postorderTraversal(currentNode = this.root, result = []) {
+        if (!currentNode) return result
+
+        console.log(currentNode)
+
+        this.postorderTraversal(currentNode.left, result)
+        this.inOrderTraversal(currentNode.right, result)
+        result.push(currentNode.value)
+
+        return result
+    }
+
+    toArray() {
+        if (this.isEmpty()) {
+            return null
+        }
+
+        let result = [this.root.value]
+
+        const queue = [this.root]
+
+        while (queue.length) {
+            const currentNode = queue.pop()
+
+            if (currentNode.right && currentNode.left) {
+                queue.push(currentNode.right)
+                queue.push(currentNode.left)
+
+                result = [...result, currentNode.left.value, currentNode.right.value]
+                continue
+            }
+
+            if (currentNode.left) {
+                queue.push(currentNode.right) 
+                result = [...result, currentNode.right.value] 
+                }
+
+            if (currentNode.right) {
+                queue.push(currentNode.right)
+                result = [...result, currentNode.right.value]
+                } 
+            }
+
+            console.log(result)
+
+            return result
+    }
+}
