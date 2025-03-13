e8794dc14eae0586bc746a05061388d73d93136e
“malcolmmutare” <mutarem@gmail.com>
2025-03-13 13:42:44 +0200

    feat: added avl-tree + tests

---
+++ src/trees/avl-tree.js
@@ -0,0 +1,177 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+        this.height = 1
+    }
+}
+
+export class AVLTree {
+    constructor() {
+        this.root = null
+    }
+
+    isEmpty() {
+        return this.root === null
+    }
+
+    getHeight(node) {
+        if (!node) {
+            return 0
+        }
+
+        return node.height
+    }
+
+    getBalanceFactor(node) {
+        if (!node) {
+            return 0
+        }
+
+        return this.getHeight(node.left) - this.getHeight(node.right)
+    }
+
+    getNewHeight (node) {
+        if (!node) {
+            return 0
+        }
+
+        return Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1
+    }
+
+    rightRotate(node) {
+        let newRoot = node.left
+        let newLeftChildOfOldRoot = newRoot.right
+
+        let oldRoot = node
+
+        node = newRoot
+
+        node.right = oldRoot
+        oldRoot.left = newLeftChildOfOldRoot
+
+        oldRoot.height = this.getNewHeight(oldRoot)
+        node.height = this.getNewHeight(node)
+
+        console.log({
+            oldRoot,
+            node
+        })
+
+        return node
+    }
+
+    leftRotate(node) {
+        let newRoot = node.right
+        let oldRoot = node
+        let newRightChildOfOldRoot = newRoot.left
+
+        node = newRoot
+        newRoot.left = oldRoot
+        oldRoot.right = newRightChildOfOldRoot
+
+        oldRoot.height = this.getNewHeight(oldRoot)
+        node.height = this.getNewHeight(node)
+
+        return node
+    }
+
+    balanceAndRotate(currentNode) {
+        currentNode.height = this.getNewHeight(currentNode)
+
+        const balanceFactor = this.getBalanceFactor(currentNode)
+
+        if (balanceFactor > 1) {
+            // If balance factor is less than 0 for left child, then it is a LR imbalance
+            // It means the current node is skewed towards the left and the left child is skewed towards the right
+            // It becomes like a zig-zag shape
+            // It that is the case, before doing the right rotation,
+            // since the current node is skewed towards the left
+            // we need to do a left rotation on the left child
+            // Because the left child is skewed towards the right
+            if (this.getBalanceFactor(currentNode.left) < 0) {
+                currentNode.left = this.leftRotate(currentNode.left)
+            }
+            return this.rightRotate(currentNode)
+        } else if (balanceFactor < -1){
+            // Same logic as above, but for the right child
+            // If the currentNode is imbalanced towards the right
+            // and the right child is imbalanced towards the left
+            // we need to do a left rotation on the right child
+            // Because the left child is skewed towards the right
+            if (this.getBalanceFactor(currentNode.right) > 0) {
+                currentNode.right = this.rightRotate(currentNode.right)
+            }
+            return this.leftRotate(currentNode)
+        }
+
+        return currentNode
+    }
+
+    insert(value, currentNode = this.root) {
+        if (!currentNode) {
+            const newNode = new Node(value)
+            if (!this.root) {
+                this.root = newNode
+            }
+            return newNode
+        }
+
+        if (value > currentNode.value) {
+            currentNode.right = this.insert(value, currentNode.right)
+        }else {
+            currentNode.left = this.insert(value, currentNode.left)
+        }
+
+        return this.balanceAndRotate(currentNode)
+    }
+
+    find(value, currentNode = this.root) {
+        if (!currentNode) {
+            return null
+        }
+
+        if (currentNode.value === value) {
+            return currentNode
+        }
+
+        if (value > currentNode.value) {
+            return this.find(value, currentNode.right)
+        }else {
+            return this.find(value, currentNode.left)
+        }
+    }
+
+    delete(value, currentNode = this.root) {
+        if (!currentNode) {
+            return currentNode
+        }
+
+        if (value > currentNode.value) {
+            currentNode.right = this.delete(value, currentNode.right)
+        }else if (value < currentNode.value) {
+            currentNode.left = this.delete(value, currentNode.left)
+        }else {
+            if (!currentNode.left) {
+                return currentNode.right
+            } else if (!currentNode.right) {
+                return currentNode.left
+            }
+
+            let minFromRightSubTree = currentNode.right
+
+            while (minFromRightSubTree.left) {
+                minFromRightSubTree = minFromRightSubTree.left
+            }
+
+            currentNode.value = minFromRightSubTree.value
+            currentNode.right = this.delete(
+                minFromRightSubTree.value,
+                currentNode.right
+            )
+        }
+
+        return this.balanceAndRotate(currentNode)
+    }
+}
---
+++ src/trees/avl-tree.test.js
@@ -0,0 +1,80 @@
+import { it, expect } from 'vitest'
+import { AVLTree } from './avl-tree'
+
+// Test for creating an AVL tree
+it('should create an ampty AVL tree', () => {
+    const avl = new AVLTree()
+    expect(avl.isEmpty()).toBe(true)
+})
+
+// Test for inserting elements
+it('should insert elements and maintain balance', () => {
+    const avl = new AVLTree()
+    avl.insert(3)
+    avl.insert(2)
+    const newRoot = avl.insert(1)
+
+    // Check balance after insertions
+    expect(newRoot.value).toBe(2) // Root should be 2 after rotations
+    expect(newRoot.left.value).toBe(1)
+    expect(newRoot.right.value).toBe(3)
+})
+
+// Test for Right Rotation (LL Imbalance)
+it('should perform right rotatio for LL imbalance', () => {
+    const avl = new AVLTree()
+    avl.insert(3)
+    avl.insert(2)
+    const newRoot = avl.insert(1)
+
+    expect(newRoot.value).toBe(2) // Right rotation should make 2 the root
+    expect(newRoot.left.value).toBe(1)
+    expect(newRoot.right.value).toBe(3)
+})
+
+// Test for Left Rotation (RR Imbalance)
+it('should perform left rotation for RR imbalance', () => {
+    const avl = new AVLTree()
+    avl.insert(1)
+    avl.insert(2)
+    const newRoot = avl.insert(3)
+
+    expect(newRoot.value).toBe(2) // Left rotation should make 2 the root
+    expect(newRoot.left.value).toBe(1)
+    expect(newRoot.right.value).toBe(3)
+})
+
+// Test for Left-Right Rotation (LR Imbalance)
+it('should perform left-right rotation for LR imbalance', () => {
+    const avl = new AVLTree()
+    avl.insert(3)
+    avl.insert(1)
+    const newRoot = avl.insert(2)
+
+    expect(newRoot.value).toBe(2) // Left-right rotation should make 2 the root
+    expect(newRoot.left.value).toBe(1)
+    expect(newRoot.right.value).toBe(3)
+})
+
+// Test for Right-Left Rotation (RL Imbalance)
+it('should perform right-left rotation for RL imbalance', () => {
+    const avl = new AVLTree()
+    avl.insert(1)
+    avl.insert(3)
+    const newRoot = avl.insert(2)
+
+    expect(newRoot.value).toBe(2) // Right-left rotation should make 2 the root
+    expect(newRoot.left.value).toBe(1)
+    expect(newRoot.right.value).toBe(3)
+})
+
+it('should delete a leaf node correctly', () => {
+    const avl = new AVLTree()
+    avl.insert(10)
+    avl.insert(5)
+    avl.insert(15)
+    avl.delete(15)
+
+    expect(avl.find(15)).toBeNull()
+    expect(avl.find(10).right).toBeNull()
+})
