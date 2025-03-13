a369510022153b16876f9c95453f35e8a040bdd4
“malcolmmutare” <mutarem@gmail.com>
2025-03-13 23:19:27 +0200

    feat: added min-heap + test

---
+++ src/heap/min-heap.js
@@ -0,0 +1,137 @@
+export class MinHeap {
+    constructor() {
+        this.heap = []
+    }
+
+    isEmpty() {
+        return this.heap.length === 0
+    }
+
+    peek() {
+        return this.heap[0]
+    }
+
+    #swap(index1, index2) {
+        let temp = this.heap[index1]
+        this.heap[index1] = this.heap[index2]
+        this.heap[index2] = temp
+    }
+
+    #getParentIndex(children) {
+        return Math.floor((children - 1) / 2)
+    }
+
+    #bubbleUp() {
+        let indexOfInsertedNode = this.heap.length - 1
+        let indexOfParentNode = this.#getParentIndex(indexOfInsertedNode)
+
+        while (true) {
+            const valueOfInsertedNode = this.heap[indexOfInsertedNode]
+            const valueOfParentNode = this.heap[indexOfParentNode]
+
+            if (valueOfParentNode > valueOfInsertedNode) {
+                this.#swap(indexOfInsertedNode, indexOfParentNode)
+
+                let tempIndex = indexOfParentNode
+                indexOfInsertedNode = indexOfParentNode
+                indexOfParentNode = this.#getParentIndex(tempIndex)
+
+                continue
+            }
+
+            break
+        }
+    }
+
+    insert(value) {
+        this.heap.push(value)
+        this.#bubbleUp()
+    }
+
+    #getLeftChildIndex(parentIndex) {
+        return 2 * parentIndex + 1
+    }
+
+    #getRightChildIndex(parentIndex) {
+        return 2 * parentIndex + 2
+    }
+
+    #bubbleDown(indexToBubbleDown = 0) {
+        while (true) {
+            const leftChildIndex = this.#getLeftChildIndex(indexToBubbleDown)
+            const rightChildIndex = this.#getRightChildIndex(indexToBubbleDown)
+
+            const currentValue = this.heap[indexToBubbleDown]
+            const leftChildValue = this.heap[leftChildIndex]
+            const rightChildValue = this.heap[rightChildIndex]
+
+            // Current having less priority than both children
+            // in min heap this means that the current node is greater than both children
+            const isCurrentLessPriorityThanBothChildren =
+                leftChildValue < currentValue && rightChildValue < currentValue
+
+            if (isCurrentLessPriorityThanBothChildren) {
+                if (leftChildValue < rightChildValue) {
+                    this.#swap(leftChildIndex, indexToBubbleDown)
+                    indexToBubbleDown = leftChildIndex
+                    continue
+                }
+
+
+                if (rightChildValue < leftChildValue) {
+                    this.#swap(rightChildIndex, indexToBubbleDown)
+                    indexToBubbleDown = rightChildValue
+                    continue
+                }
+            }
+
+            if (leftChildValue < currentValue) {
+                this.#swap(leftChildIndex, indexToBubbleDown)
+                indexToBubbleDown = leftChildIndex
+                continue
+            }
+
+            if (rightChildValue < currentValue) {
+                this.#swap(rightChildIndex, indexToBubbleDown)
+                indexToBubbleDown = rightChildIndex
+                continue
+            }
+
+            break
+        }
+    }
+
+    size() {
+        return this.heap.length
+    }
+
+    remove() {
+        if (this.heap.length === 1) {
+            return this.heap.pop()
+        }
+
+        const lastItem = this.heap.pop()
+        const firstItem = this.heap[0]
+
+        this.heap[0] = lastItem
+        this.#bubbleDown()
+
+        return firstItem
+    }
+
+    #getIndexOfLastNonLeafNode() {
+        const indexOfLastItem = this.heap.length - 1
+        return this.#getParentIndex(indexOfLastItem)
+    }
+
+    heapify(array) {
+        this.heap = array
+        
+        let indexOfLastNonLeafNode = this.#getIndexOfLastNonLeafNode()
+        
+        while (indexOfLastNonLeafNode >= 0) {
+            this.#bubbleDown(indexOfLastNonLeafNode)
+            indexOfLastNonLeafNode--
+        }
+    }
+}
---
+++ src/heap/min-heap.test.js
@@ -0,0 +1,43 @@
+import { it, expect } from 'vitest'
+import { MinHeap } from './min-heap'
+
+// Test for creating a new min heap
+it('should create an empty min heap', () => {
+    const minHeap = new MinHeap()
+    expect(minHeap).toBeDefined()
+    expect(minHeap.isEmpty()).toBe(true)
+})
+
+// Test for inserting elements 
+it('it should insert elements and maintain min heap property', () => {
+    const minHeap = new MinHeap()
+    minHeap.insert(3)
+    minHeap.insert(1)
+    minHeap.insert(6)
+
+    expect(minHeap.peek()).toBe(1)
+})
+
+// Test for removing the root element
+it('should remove the root element and maintain min heap property', () => {
+    const minHeap = new MinHeap()
+    minHeap.insert(3)
+    minHeap.insert(1)
+    minHeap.insert(6)
+    expect(minHeap.remove()).toBe(1)
+    expect(minHeap.peek()).toBe(3)
+})
+
+it('should correctly heapify an array', () => {
+    const minHeap = new MinHeap()
+    minHeap.heapify([3, 1, 6])
+    expect(minHeap.peek()).toBe(1)
+})
+
+it('should spit out size of head', () => {
+    const minHeap = new MinHeap()
+    minHeap.insert(3)
+    minHeap.insert(1)
+    minHeap.insert(6)
+    expect(minHeap.size()).toBe(3)
+})
