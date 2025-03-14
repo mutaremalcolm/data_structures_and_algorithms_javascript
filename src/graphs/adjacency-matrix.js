f30f5ea2a023a44842e6234c53676eec048371ed
“malcolmmutare” <mutarem@gmail.com>
2025-03-14 20:01:19 +0200

    feat: added adjency-matrix + test

---
+++ src/graphs/adjacency-matrix.js
@@ -0,0 +1,23 @@
+export class AdjacencyMatrix {
+    constructor(numberOfVertices) {
+        this.numberOfVertices = numberOfVertices
+        // 2D Array
+        this.matrix = Array.from({ length: numberOfVertices }, () => 
+            Array.from({ length: numberOfVertices }, () => 0)
+        )
+    }
+
+    addEdge(vertex1, vertex2) {
+        this.matrix[vertex1][vertex2] = 1
+        this.matrix[vertex2][vertex1] = 1
+    }
+
+    removeEdge(vertex1, vertex2) {
+        this.matrix[vertex1][vertex2] = 0
+        this.matrix[vertex2][vertex1] = 0
+    }
+
+    hasEdge(vertex1, vertex2) {
+        return this.matrix[vertex1][vertex2] === 1
+    }
+}
