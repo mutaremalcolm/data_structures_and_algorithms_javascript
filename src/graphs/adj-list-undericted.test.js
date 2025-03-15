b60fa6cdc8bf5e8e009ccb20d519b5469cd93a3c
“malcolmmutare” <mutarem@gmail.com>
2025-03-15 22:10:16 +0200

    feat: added adj-list-undirected + test

---
+++ src/graphs/adj-list-undericted.test.js
@@ -0,0 +1,91 @@
+import { it, expect } from 'vitest'
+import { AdjListUndirected } from './adj-list-undirected'
+
+// Test for creating a new graph 
+it('should create a new graph', () => {
+    const graph = new AdjListUndirected()
+    expect(graph).toBeDefined()
+})
+
+// Test for adding a vertex
+it('it should add vertices correctly', () => {
+    const graph = new AdjListUndirected()
+    graph.addVertex('A')
+    graph.addVertex('B')
+    expect(graph.hasVertex('A')).toBe(true)
+    expect(graph.hasVertex('B')).toBe(true)
+    expect(graph.hasVertex('C')).toBe(false)
+})
+
+// Test for adding an edge
+it('should remove an edge correctly', () => {
+    const graph = new AdjListUndirected()
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addEdge('A', 'B')
+    graph.removeEdge('A', 'B')
+    expect(graph.hasEdge('A', 'B')).toBe(false)
+    expect(graph.hasEdge('B', 'A')).toBe(false) // Undirected graph
+})
+
+// Test for removing an edge
+it('should remove an edge correctly', () => {
+    const graph = new AdjListUndirected()
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addEdge('A', 'B')
+    graph.removeEdge('A', 'B')
+    expect(graph.hasEdge('A', 'B')).toBe(false)
+    expect(graph.hasEdge('B', 'A')).toBe(false) // Undirected graph
+})
+
+// Test for ensuring that non-existing edges are handled correctly
+it('should handle non-existing edges correctly', () => {
+    const graph = new AdjListUndirected()
+    graph.addVertex('A')
+    graph.addVertex('B')
+    expect(graph.hasEdge('A', 'B')).toBe(false)
+})
+
+// Setup a sample graph
+function createSampleGraph() {
+    const graph = new AdjListUndirected()
+    // A graph that looks like:
+    //     A
+    //   /   \
+    //  B     C
+    //  |     |
+    //  D --- E
+    //       / \
+    //      F   G
+    graph.addVertex('A')
+    graph.addVertex('B')
+    graph.addVertex('C')
+    graph.addVertex('D')
+    graph.addVertex('E')
+    graph.addVertex('F')
+    graph.addVertex('G')
+    graph.addEdge('A', 'B')
+    graph.addEdge('A', 'C')
+    graph.addEdge('B', 'D')
+    graph.addEdge('C', 'E')
+    graph.addEdge('D', 'E')
+    graph.addEdge('E', 'F')
+    graph.addEdge('E', 'G')
+    return graph
+  }
+
+
+// Test for Breadth-First Search (BFS)
+it('should correctly perform BFS traversal', () => {
+    const graph = createSampleGraph()
+    const dfsResult = graph.dfs('A')
+    expect(dfsResult).toEqual(['A', 'B', 'D', 'E', 'C', 'F', 'G'])
+})
+
+// Test for Breadth-First Search (BFS)
+it('should correctly perform BFS traversal', () => {
+    const graph = createSampleGraph()
+    const bfsResult = graph.bfs('A')
+    expect(bfsResult).toEqual(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+})
---
+++ src/graphs/adj-list-undirected.js
@@ -0,0 +1,91 @@
+export class AdjListUndirected {
+    constructor() {
+        this.list = new Map()
+    }
+
+    addVertex(vertex) {
+        const isExistingVertex = this.list.has(vertex)
+
+        if (isExistingVertex) throw new Error('Vertex already exists')
+
+            this.list.set(vertex, [])
+    }
+
+    hasVertex(vertex) {
+        const isExistingVertex = this.list.has(vertex)
+        return isExistingVertex
+    }
+
+    addEdge(vertex1, vertex2) {
+        const edgesForVertex1 = this.list.get(vertex1)
+        const edgesForVertext2 = this.list.get(vertex2)
+
+        const newEdgesForVertex1 = [...edgesForVertex1, vertex2]
+        const newEdgesForVertex2 = [...edgesForVertext2, vertex1]
+
+        this.list.set(vertex1, newEdgesForVertex1)
+        this.list.set(vertex2, newEdgesForVertex2)
+    }
+
+    hasEdge(vertex1, vertex2) {
+        const edgesForVertex1 = this.list.get(vertex1)
+        const edgesForVertext2 = this.list.get(vertex2)
+
+        const isVertex1InVertex2Edges = edgesForVertext2.includes(vertex1)
+        const isVertex2InVertex1Edges = edgesForVertex1.includes(vertex2)
+
+        return isVertex1InVertex2Edges && isVertex2InVertex1Edges
+    }
+
+    removeEdge(vertex1, vertex2) {
+        const edgesForVertex1 = this.list.get(vertex1)
+        const edgesForVertext2 = this.list.get(vertex2)
+
+        const newEdgesForVertex1 = edgesForVertex1.filter(
+            (edge) => edge !== vertex2
+        )
+        const newEdgesForVertex2 = edgesForVertext2.filter(
+            (edge) => edge !== vertex1
+        )
+
+        this.list.set(vertex1, newEdgesForVertex1)
+        this.list.set(vertex2, newEdgesForVertex2)
+     }
+
+     dfs(startingVertex, visited = new Set(), result = []) {
+        if (!visited.has(startingVertex)) {
+            visited.add(startingVertex)
+            result.push(startingVertex)
+        }
+
+     const edges = this.list.get(startingVertex)
+     edges.forEach((edge) => {
+        if (!visited.has(edge)) {
+            this.dfs(edge, visited, result)
+        }
+     })
+
+     return result
+     }
+
+     bfs(startingVertex) {
+        const visited = new Set()
+        const queue = [startingVertex]
+        visited.add(startingVertex)
+        const result = [startingVertex]
+
+        while (queue.length) {
+            const currentVertex = queue.shift()
+            const edges = this.list.get(currentVertex)
+            edges.forEach((edge) => {
+                if (!visited.has(edge)) {
+                    result.push(edge)
+                    visited.add(edge)
+                    queue.push(edge)
+                }
+            })
+        }
+
+        return result
+     }
+}
